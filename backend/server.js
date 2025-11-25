bd4f085f0355c785045dbf368c3cdc6de3f5b7ce
nSzandi <nagy.alexandra.laura@gmail.com>
2025-11-25 18:28:05 +0100

    backend és frontend modositas sikeres

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../frontend/public')));
+app.use('/backend/scripts', express.static(path.join(__dirname, 'scripts')));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/public/index.html"));
